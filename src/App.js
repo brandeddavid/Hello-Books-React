7a1350678961acf5f57acc9839ef5c3e9c9b7288
brandeddavid <david.mathenge98@gmail.com>
2018-08-03 22:40:34 +0300

    [FT #159475324]Add user and admin dashboard components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Login from "./components/login/login"
 import Index from "./components/index/index"
 import Register from "./components/register/register"
+import AdminDash from "./components/dashboards/admin"
+import UserDash from "./components/dashboards/user"
 import { Router, Route } from "react-router-dom"
 import history from "./utils/history";
 
@@ -14,6 +16,8 @@ class App extends Component {
             <Route exact path="" component={Index} ></Route>
             <Route path="/login" component={Login} ></Route>
             <Route path="/register" component={Register} ></Route>
+            <PrivateRoute path="/admin"> component={AdminDash}></PrivateRoute>
+            <PrivateRoute path="/user"> component={UserDash}></PrivateRoute>
           </div>
         </Router>
       </div>
