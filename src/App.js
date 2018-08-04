ddaad75b03fc155fdc83aa6dac7a73d472a527be
David Mwangi <david.mathenge98@gmail.com>
2018-08-04 21:57:19 +0300

    Revert "Ch styling 159542873"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
-import Login from "./components/login/login"
-import Index from "./components/index/index"
-import Register from "./components/register/register"
-import AdminDash from "./components/dashboards/admin"
-import UserDash from "./components/dashboards/user"
-import { Router, Route } from "react-router-dom"
-import history from "./utils/history";
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Router history={history}>
-          <div>
-            <Route exact path="" component={Index} ></Route>
-            <Route path="/login" component={Login} ></Route>
-            <Route path="/register" component={Register} ></Route>
-            <Route path="/admin" component={AdminDash}></Route>
-            <Route path="/user" component={UserDash}></Route>
-          </div>
-        </Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
