ba3dbc2f65e052f0bd1744d9f7a03c6cdfce11d0
brandeddavid <david.mathenge98@gmail.com>
2018-08-06 10:54:13 +0300

    [FT #159554240]Secure admin and user dashboards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import { Router, Route } from "react-router-dom";
 import Login from "./components/login/login";
 import Index from "./components/index/index";
 import Register from "./components/register/register";
 import Library from "./components/library/library";
 import AdminDash from "./components/dashboards/admin";
 import UserDash from "./components/dashboards/user";
-import { Router, Route } from "react-router-dom";
+import PrivateRoute from "./utils/privateRoutes"
 import history from "./utils/history";
 
 class App extends Component {
@@ -18,8 +19,8 @@ class App extends Component {
             <Route path="/login" component={Login} />
             <Route path="/register" component={Register} />
             <Route path="/library" component={Library} />
-            <Route path="/admin" component={AdminDash} />
-            <Route path="/user" component={UserDash} />
+            <PrivateRoute path="/admin" component={AdminDash} />
+            <PrivateRoute path="/user" component={UserDash} />
           </div>
         </Router>
       </div>
