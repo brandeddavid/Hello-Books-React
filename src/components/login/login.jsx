7e6fcb7b9fdce662f72f9c95af0bea976a1ceaf3
brandeddavid <david.mathenge98@gmail.com>
2018-08-12 21:28:08 +0300

    [FT #159626135]Remove axios library

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import axios from "axios";
 import { Redirect } from "react-router-dom";
 import IndexNav from "../navbars/indexnav";
 import "../../static/css/forms.css";
