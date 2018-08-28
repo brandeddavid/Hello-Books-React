4d736cefbe8c7710bc4b071734fe2d45fd544976
brandeddavid <david.mathenge98@gmail.com>
2018-08-28 12:11:17 +0300

    [Feature]Add user book history component

---
+++ src/components/user/history/borrowHistory.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import UserNav from "../../navbars/usernav"
+class BorrowHistory extends Component {
+  componentDidMount() {
+    this.props.borrowHistory();
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <UserNav/>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="text-center">
+                <h1>Borrow History</h1>
+              </div>
+              <div className="table-responsive">
+                <table className="table table-hover">
+                  <thead className="thead-dark">
+                    <th>Book Title</th>
+                    <th>Book Author</th>
+                    <th>Book ISBN</th>
+                    <th>Date Borrowed</th>
+                    <th>Date Due</th>
+                    <th>Date Returned</th>
+                  </thead>
+                  <tbody>
+                  {this.props.borrowedBooksHistory.map(book => (
+                    <tr key={book.id}>
+                      <td>{book.title}</td>
+                      <td>{book.author}</td>
+                      <td>{book.isbn}</td>
+                      <td>{book.borrowDate}</td>
+                      <td>{book.dueDate ? book.dueDate : "Returned"}</td>
+                      <td>{book.returnDate ? book.returnDate : "Not Returned"}</td>
+                    </tr>
+                  ))}
+                </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default BorrowHistory;
