54b1e43661fece9e6ea1ae86ecd3db5812dc91ae
brandeddavid <david.mathenge98@gmail.com>
2018-09-04 23:04:35 +0300

    [Feature]App tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import {mount} from 'enzyme';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { mount, shallow } from "enzyme";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
-it('renders one route at a time', () => {
-  const app = mount(<App />)
-  expect(app.length).toEqual(1)
-})
+it("renders one route at a time", () => {
+  const app = mount(<App />);
+  expect(app).toHaveLength(1);
+});
--- src/pollyFills.js
+++
@@ -1,5 +0,0 @@
-const polly = (global.requestAnimationFrame = callback => {
-  setTimeout(callback, 0);
-});
-
-export default polly;
